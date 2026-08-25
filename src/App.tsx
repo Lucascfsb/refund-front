da36d1946116e1a6b9a6737ecc0aa49705a7b3de
Lucas Castro <lucascfsb@gmail.com>
2026-08-25 16:39:37 -0300

    feat: add routing with AuthLayout and SignIn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import { Routes } from "./routes";
+
 export function App() {
-  return (
-    <div>
-      <h1 className="text-3xl text-green-200 font-bold">Hello, World!</h1>
-    </div>
-  );
+  return <Routes />;
 }
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,7 @@
+export function SignIn() {
+  return (
+    <div>
+      <h1 className="text-3xl text-green-200 font-bold">Sign In Page</h1>
+    </div>
+  );
+}
