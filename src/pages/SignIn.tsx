4d10602f9637678eb320f24974db96e7da52291d
Lucas C <lucascfsb@gmail.com>
2026-08-26 17:55:49 -0300

    feat: enhance SignIn form with submission handling and loading state

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(event: React.SubmitEvent) {
+    event.preventDefault();
+    alert("Formulário enviado!");
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         placeholder="seu@email.com"
         legend="E-mail"
         type="email"
+        onChange={(event) => setEmail(event.target.value)}
       />
-      <Input required placeholder="123456" legend="Senha" type="password" />
+      <Input
+        required
+        placeholder="123456"
+        legend="Senha"
+        type="password"
+        onChange={(event) => setPassword(event.target.value)}
+      />
 
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
     </form>
   );
 }
