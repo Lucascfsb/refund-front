09e2cbc10eebd0b5a6eec7f8412b1a37e00cf38f
Lucas Castro <lucascfsb@gmail.com>
2026-08-29 19:54:54 -0300

    feat: implement formatCurrency utility and update Dashboard to use formatted amounts

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import searchSvg from "../assets/search.svg";
 
+import { CATEGORIES } from "../utils/categories";
+import { formatCurrency } from "../utils/formatCurrency";
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { RefundItem } from "../components/RefundItem";
-import { CATEGORIES } from "../utils/categories";
 
 const REFUND_EXAMPLE = {
   id: "123",
   name: "João da Silva",
   category: "Transporte",
-  amount: "3.000,00",
+  amount: formatCurrency(3.0),
   categoryImg: CATEGORIES["transport"].icon,
 };
 
